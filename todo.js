90a1f086a1b16ad08f91996c0d028daf5885e05e
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2024-03-25 14:22:10 +0530

    Done

--- todo.js
+++ todo.js
@@ -1,16 +1,33 @@
 let todoList = [];
 
-displayItems();
+if (localStorage.getItem("items")) {
+  todoList = JSON.parse(localStorage.getItem("items"));
+  displayItems();
+  toggleClearButton();
+}
 
 function addTodo() {
   let inputElement = document.querySelector("#todo-input");
   let dateElement = document.querySelector("#todo-date");
   let todoDate = dateElement.value;
   let todoItem = inputElement.value;
+
+  if (todoItem.trim() === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  if (todoDate === "") {
+    alert("Please select a due date");
+    return;
+  }
+
   todoList.push({ item: todoItem, dueDate: todoDate });
   inputElement.value = "";
   dateElement = "";
+  localStorage.setItem("items", JSON.stringify(todoList));
   displayItems();
+  toggleClearButton();
 }
 
 function displayItems() {
@@ -19,14 +36,45 @@ function displayItems() {
   let newHtml = "";
 
   for (let i = 0; i < todoList.length; i++) {
-    let { item, dueDate } = todoList[i];
+    let { item, dueDate, completed } = todoList[i];
+    let completedClass = completed ? "completed" : "";
     newHtml += `
-
-    <span>${item}</span>
-    <span>${dueDate}</span>
-    <button class="btn-delete" onclick='todoList.splice(${i},1); displayItems();'>Delete</button>
-
+            <input type="checkbox" onchange="toggleCompletion(${i})" ${
+      completed ? "checked" : ""
+    }>
+            <span class="${completedClass}">${item}</span>
+            <span>${dueDate}</span>
+            <button class="btn-delete" onclick="deleteItem(${i})">Delete</button>
     `;
   }
   containerElement.innerHTML = newHtml;
+}
+
+function toggleCompletion(index) {
+  todoList[index].completed = !todoList[index].completed;
+  localStorage.setItem("items", JSON.stringify(todoList));
+  displayItems();
+}
+
+function deleteItem(index) {
+  todoList.splice(index, 1);
+  localStorage.setItem("items", JSON.stringify(todoList));
+  displayItems();
+  toggleClearButton();
+}
+
+function toggleClearButton() {
+  let clearButton = document.getElementById("clear-button");
+  if (todoList.length > 0) {
+    clearButton.style.display = "block";
+  } else {
+    clearButton.style.display = "none";
+  }
+}
+
+function clearAll() {
+  todoList.splice(0, todoList.length);
+  localStorage.setItem("items", JSON.stringify(todoList));
+  displayItems();
+  toggleClearButton();
 }
